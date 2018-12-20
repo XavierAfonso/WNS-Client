9d87de004bef1e88a52accf55b48df1ffa481fc0
XavierAfonso <xavier.vazafonso@heig-vd.ch>
2018-12-20 14:55:44 +0100

    Add Dialog example

--- src/WNS/ContentProfil.js
+++ src/WNS/ContentProfil.js
@@ -7,6 +7,13 @@ import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import Post from '../Components/Post';
 
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 
 const styles = theme => ({
 
@@ -55,8 +62,18 @@ class Content extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      open: false,
     }
   }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
 
   componentDidMount() {
     window.scrollTo(0, 0)
@@ -87,9 +104,39 @@ class Content extends React.Component {
           <Paper className={classes.newPost}>
 
 
-          <Button variant="contained" color="primary" className={classes.addBtn}>
+          <Button variant="contained" color="primary" className={classes.addBtn} onClick={this.handleClickOpen}>
           Primary
          </Button>  
+
+         <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address here. We will send
+              updates occasionally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Subscribe
+            </Button>
+          </DialogActions>
+        </Dialog>
 
           </Paper>
 
