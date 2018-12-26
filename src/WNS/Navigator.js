cd92183ce42c0bcba786eebb89521387d301df7e
XavierAfonso <xavier.vazafonso@heig-vd.ch>
2018-12-26 19:43:04 +0100

    Clean olds cods

--- src/WNS/Navigator.js
+++ src/WNS/Navigator.js
@@ -2,31 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import PeopleIcon from '@material-ui/icons/People';
-import DnsRoundedIcon from '@material-ui/icons/DnsRounded';
-import PermMediaOutlinedIcon from '@material-ui/icons/PhotoSizeSelectActual';
-import PublicIcon from '@material-ui/icons/Public';
-import SettingsEthernetIcon from '@material-ui/icons/SettingsEthernet';
-import SettingsInputComponentIcon from '@material-ui/icons/SettingsInputComponent';
-import TimerIcon from '@material-ui/icons/Timer';
-import SettingsIcon from '@material-ui/icons/Settings';
-import PhonelinkSetupIcon from '@material-ui/icons/PhonelinkSetup';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
-import Chip from '@material-ui/core/Chip';
-import Paper from '@material-ui/core/Paper';
-import TagFacesIcon from '@material-ui/icons/TagFaces';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
-
 import Button from '@material-ui/core/Button';
 import Search from '@material-ui/icons/Search';
 
 import ChipInput from 'material-ui-chip-input';
-
-
 import AutoComplete from '../Pages/AutoComplete';
 
 const authors = [
@@ -130,14 +113,6 @@ class Navigator extends React.Component {
       title: "",
       author: "",
       tags: "",
-      idChip: 0,
-      chipData: [
-        /*{ key: 0, label: 'Angular' },
-        { key: 1, label: 'jQuery' },
-        { key: 2, label: 'Polymer' },
-        { key: 3, label: 'React' },
-        { key: 4, label: 'Vue.js' },*/
-      ],
       name: [],
       multiline: '',
     }
@@ -218,24 +193,6 @@ class Navigator extends React.Component {
             <Input id="title" name="title" autoComplete="title" onChange={this.handleInputChange} />
           </FormControl>
 
-          
-          {/*<FormControl margin="normal" fullWidth>
-            <InputLabel htmlFor="select-multiple">Authors</InputLabel>
-            <Select
-              multiple
-              value={this.state.name}
-              onChange={this.handleChangeAuthors}
-              input={<Input id="select-multiple" />}
-            //MenuProps={MenuProps}
-            >
-              {names.map(name => (
-                <MenuItem key={name} value={name}>
-                  {name}
-                </MenuItem>
-              ))}
-            </Select>
-              </FormControl>*/}
-
           <FormControl margin="normal" fullWidth>
             <TextField
               id="standard-multiline-flexible"
@@ -251,135 +208,30 @@ class Navigator extends React.Component {
 
 
           <FormControl margin="normal" fullWidth>
-          <AutoComplete label = "Authors" data = {authors} />
+            <AutoComplete label = "Authors" data = {authors} />
           </FormControl>
 
           <FormControl margin="normal" fullWidth>
-          <AutoComplete label = "Languages" data = {languages} />
+            <AutoComplete label = "Languages" data = {languages} />
           </FormControl>
 
-          {/*<FormControl margin="normal" fullWidth>
-            <InputLabel htmlFor="tags">Tags</InputLabel>
-            <Input id="tags" name="tags" autoComplete="tags" onChange={this.handleInputChange}
-              onKeyPress={(ev) => {
-                console.log(`Pressed keyCode ${ev.key}`);
-                if (ev.key === 'Enter') {
-                  // Do code here
-                  ev.preventDefault();
-                  this.coucou();
-                }
-              }}
-
-            />
-            </FormControl>
-
-          <Paper className={classes.genres}>
-
-            {this.state.chipData.map(data => {
-              let icon = null;
-
-              if (data.label === 'React') {
-                icon = <TagFacesIcon />;
-              }
-
-              return (
-                <Chip
-                  key={data.key}
-                  icon={icon}
-                  label={data.label}
-                  onDelete={this.handleDelete(data)}
-                  className={classes.chip}
-                />
-              );
-            })}
-          </Paper>*/}
-
-
           <FormControl margin="normal" fullWidth>
-
-          
             <ChipInput
               label="Tags"
               variant='standard'
               onChange={(chips) => this.handleChange(chips)}
             />
-            
           </FormControl>
 
 
           <FormControl margin="normal" fullWidth>
             <Button variant="contained" color="primary" className={classes.button}>
               Search
-        <Search className={classes.rightIcon} />
+                <Search className={classes.rightIcon} />
             </Button>
           </FormControl>
-
-
-          
-        
- 
-       
-
-
         </div>
-
-        {/*<List disablePadding>
-        <ListItem className={classNames(classes.firebase, classes.item, classes.itemCategory)}>
-          Paperbase
-        </ListItem>
-        <ListItem className={classNames(classes.item, classes.itemCategory)}>
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText
-            classes={{
-              primary: classes.itemPrimary,
-            }}
-          >
-            Project Overview
-          </ListItemText>
-        </ListItem>
-        {categories.map(({ id, children }) => (
-          <React.Fragment key={id}>
-            <ListItem className={classes.categoryHeader}>
-              <ListItemText
-                classes={{
-                  primary: classes.categoryHeaderPrimary,
-                }}
-              >
-                {id}
-              </ListItemText>
-            </ListItem>
-            {children.map(({ id: childId, icon, active }) => (
-              <ListItem
-                button
-                dense
-                key={childId}
-                className={classNames(
-                  classes.item,
-                  classes.itemActionable,
-                  active && classes.itemActiveItem,
-                )}
-              >
-                <ListItemIcon>{icon}</ListItemIcon>
-                <ListItemText
-                  classes={{
-                    primary: classes.itemPrimary,
-                    textDense: classes.textDense,
-                  }}
-                >
-                  {childId}
-                </ListItemText>
-              </ListItem>
-            ))}
-            <Divider className={classes.divider} />
-          </React.Fragment>
-        ))}
-      </List>
-
-                */}
       </Drawer>
-
     );
   }
 }
